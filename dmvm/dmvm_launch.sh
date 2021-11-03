2afafe562b89ab93dd2b2f13871412758f940c6b
Thomas Gruber <Thomas.Roehl@googlemail.com>
2021-11-03 23:31:59 +0100

    Set skip mask and add variable for marker setting

--- dmvm/dmvm_launch.sh
+++ dmvm/dmvm_launch.sh
@@ -23,19 +23,27 @@ module list
 
 unset LIKWID_FORCE
 
+LIKWID_MARKERS=
+
 ## OpenMP:
-echo "Instruction count"
-for T in {2..$SLURM_CPUS_PER_TASK..2}; do
-    export OMP_NUM_THREADS=$T
-    likwid-mpirun -np 1 -t $T -g CLOCK ./matrix 2>&1 | grep "INST_RETIRED_ANY" | grep -v STAT
+echo "#######################################"
+echo "# Instruction count"
+echo "#######################################"
+for T in 1 2 4 6 8; do
+    echo -n "$T thread(s) |"
+    likwid-mpirun -s 0x0 $(LIKWID_MARKERS) -np 1 -t $T -g CLOCK ./matrix 2>&1 | grep "INSTR_RETIRED_ANY" | grep -v STAT
 done
-echo "L1 <-> L2 data volume (in GByte)"
-for T in {2..$SLURM_CPUS_PER_TASK..2}; do
-    export OMP_NUM_THREADS=$T
-    likwid-mpirun -np 1 -t $T -g L2 ./matrix 2>&1 | grep "L2 data volume" | grep -v STAT
+echo "#######################################"
+echo "# L1 <-> L2 data volume (in GByte)"
+echo "#######################################"
+for T in 1 2 4 6 8; do
+    echo -n "$T thread(s) |"
+    likwid-mpirun -s 0x0 $(LIKWID_MARKERS) -np 1 -t $T -g L2 ./matrix 2>&1 | grep "L2 data volume" | grep -v STAT
 done
-echo "L1 <-> L2 data volume (in GByte)"
-for T in {2..$SLURM_CPUS_PER_TASK..2}; do
-    export OMP_NUM_THREADS=$T
-    likwid-mpirun -np 1 -t $T -g FLOPS_AVX ./matrix 2>&1 | grep "Packed DP" | grep -v STAT
+echo "#######################################"
+echo "# DP FLOP rate"
+echo "#######################################"
+for T in 1 2 4 6 8; do
+    echo -n "$T thread(s) |"
+    likwid-mpirun -s 0x0 $(LIKWID_MARKERS) -np 1 -t $T -g FLOPS_AVX ./matrix 2>&1 | grep "Packed DP" | grep -v STAT
 done
