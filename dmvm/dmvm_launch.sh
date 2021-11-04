f1cad7af2b0a37721434513a4c69aad4288fa2b5
Thomas Roehl <thomas.roehl@fau.de>
2021-11-04 11:55:13 +0100

    Fix for LIKWID_MARKERS variable

--- dmvm/dmvm_launch.sh
+++ dmvm/dmvm_launch.sh
@@ -32,19 +32,19 @@ echo "# Instruction count"
 echo "#######################################"
 for T in 1 2 4 6 8; do
     echo -n "$T thread(s) |"
-    likwid-mpirun -s 0x0 $(LIKWID_MARKERS) -np 1 -t $T -g CLOCK ./matrix 2>&1 | grep "INSTR_RETIRED_ANY" | grep -v STAT
+    likwid-mpirun -s 0x0 ${LIKWID_MARKERS} -np 1 -t $T -g CLOCK ./matrix 2>&1 | grep "INSTR_RETIRED_ANY" | grep -v STAT
 done
 echo "#######################################"
 echo "# L1 <-> L2 data volume (in GByte)"
 echo "#######################################"
 for T in 1 2 4 6 8; do
     echo -n "$T thread(s) |"
-    likwid-mpirun -s 0x0 $(LIKWID_MARKERS) -np 1 -t $T -g L2 ./matrix 2>&1 | grep "L2 data volume" | grep -v STAT
+    likwid-mpirun -s 0x0 ${LIKWID_MARKERS} -np 1 -t $T -g L2 ./matrix 2>&1 | grep "L2 data volume" | grep -v STAT
 done
 echo "#######################################"
 echo "# DP FLOP rate"
 echo "#######################################"
 for T in 1 2 4 6 8; do
     echo -n "$T thread(s) |"
-    likwid-mpirun -s 0x0 $(LIKWID_MARKERS) -np 1 -t $T -g FLOPS_AVX ./matrix 2>&1 | grep "Packed DP" | grep -v STAT
+    likwid-mpirun -s 0x0 ${LIKWID_MARKERS} -np 1 -t $T -g FLOPS_AVX ./matrix 2>&1 | grep "Packed DP" | grep -v STAT
 done
